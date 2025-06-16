1dc8b9b9c553e3c7561cccc62f126e92999031ab
alexandre <alexandre.chevalier@laplateforme.io>
2025-06-16 15:05:43 +0200

    initialisation of the file build and display

---
+++ build-game.py
@@ -0,0 +1,14 @@
+class Build_games:
+    def __init__(self):
+        pass
+
+    def choose_difficulty(self):
+        print("lol")
+    
+    def grid_level(self):
+        print("lol")
+    
+    def move(self):
+        print("move")
+    
+    
